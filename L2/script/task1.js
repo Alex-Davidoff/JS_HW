3be97b0ca04d676a6e9c36e89948e278105658b6
Alex-Davidoff <alex.davidoff0009@gmail.com>
2024-11-04 20:36:07 +0200

    L2 t1

---
+++ L2/script/task1.js
@@ -0,0 +1,17 @@
+// #67kfznmiMl
+// - Створити масив, наповнити його 10 елементами будь-якого типу, вивести кожен елемент в консоль
+console.log("task 1 :");
+
+let arrt1 = ['hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7];
+console.log(arrt1[0]);
+console.log(arrt1[1]);
+console.log(arrt1[2]);
+console.log(arrt1[3]);
+console.log(arrt1[4]);
+console.log(arrt1[5]);
+console.log(arrt1[6]);
+console.log(arrt1[7]);
+console.log(arrt1[8]);
+console.log(arrt1[9]);
+
+console.log("-------------------------------");
