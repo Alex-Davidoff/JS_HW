b4fc53371284c1c23217f44c41c7c9d17809935f
Alex-Davidoff <alex.davidoff0009@gmail.com>
2024-11-04 20:42:42 +0200

    L2 t2

---
+++ L2/script/task2.js
@@ -0,0 +1,25 @@
+// #LARqoUj5I
+// - Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre.
+console.log("task 2 :");
+
+    let book1 = {
+        title : "test title 1",
+        pageCount : 10,
+        genre : "fantastic"  //mb arr?
+    };
+    let book2 = {
+        title : "test title 2",
+        pageCount : 20,
+        genre : "fantastic"  
+    };
+    let book3 = {
+        title : "test title 3",
+        pageCount : 30,
+        genre : "fantasy"  
+    };
+
+console.log(book1);
+console.log(book2);
+console.log(book3);
+
+console.log("-------------------------------");
