12d9ee93a1b969eddbbab262111ab57394bb6c5c
Alex-Davidoff <alex.davidoff0009@gmail.com>
2024-11-09 18:49:13 +0200

    L4 t2 1

--- L4/task2_ETGAxbEn8l/js/ETGAxbEn8l.js
+++ L4/task2_ETGAxbEn8l/js/ETGAxbEn8l.js
@@ -5,6 +5,7 @@ function calc_circle_area(r){
 let r=20;
 let circle_area = calc_circle_area(r);
 
+console.log(r,circle_area);
 document.write(`
     <p>r = ${r}</p>
     <p>circle_area = ${circle_area}</p>
