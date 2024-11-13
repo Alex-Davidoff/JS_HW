089112250885c7e4c3401fb4b93b14c585fc52c2
Alex-Davidoff <alex.davidoff0009@gmail.com>
2024-11-13 12:39:52 +0200

    L5 t4

--- L5/task4_SIdMd0hQ/js/SIdMd0hQ.js
+++ L5/task4_SIdMd0hQ/js/SIdMd0hQ.js
@@ -1,30 +1,28 @@
 let temp_arr = [];
 
-function temp_arr_init(len){
-    temp_arr = [];
-    for (let c = 0; c<len; c++){
-        let user = {};
-        user.id = c;
-        user.name = 'user_name_' + (c+1);
-        user.genre = (c % 2 === 0) ? 'M' : 'F';
-        user.status_active = (c % 3 === 0) ? false : true;
-        temp_arr.push(user);
+function* arrItemGen(){
+    let items = [0,11,22,33,44,55];
+    for (const outItem of items){
+        yield outItem;
     }
 }
 
-temp_arr_init(12);
+const arrItemVal = arrItemGen();
+
+function temp_arr_init(){
+    temp_arr = [];
+    for (let c = 0; c<6; c++){
+        temp_arr.push(arrItemVal.next().value);
+    }
+}
+
+temp_arr_init();
 console.log(temp_arr);
 
-function arr_show(inp_arr, class_name){
-    document.write('<div class="'+class_name+'">');
-    for (const t_obj of inp_arr){
-        document.write('<div class="'+class_name+'_element'+'">');
-        for (const t_key in t_obj){
-            document.write('<p>'+t_key+" : "+t_obj[t_key]+'</p>');
-        }
-        document.write('</div>');
+function arr_show (inp_arr) {
+    for (const t_item of inp_arr){
+        console.log(t_item);
     }
-    document.write('</div>');
-}
+} 
 
-arr_show(temp_arr, 'arr');
+arr_show(temp_arr);
