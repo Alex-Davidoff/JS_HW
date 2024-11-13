90c6e5155af44d62af22453aefbdfbed8b4415b9
Alex-Davidoff <alex.davidoff0009@gmail.com>
2024-11-13 15:29:05 +0200

    L5 t11

--- L5/task11_EKRNVPM/js/EKRNVPM.js
+++ L5/task11_EKRNVPM/js/EKRNVPM.js
@@ -1,17 +1,16 @@
-let t_arr = [];
-
 function t_arr_init(l){
-    t_arr = [];
+    inp_arr = [];
     for (let c=0; c<l; c++){
         let el = Math.pow(2,c);
-        t_arr.push(el);    
+        inp_arr.push(el);    
     }
+    return inp_arr;
 }
 
-t_arr_init(8,0);
+let t_arr = t_arr_init(8);
 console.log(t_arr);
 
-function sum_arr(arr){
+let sum_arr = (arr) => {
     let tsum = 0;
     for (const el of arr){
         tsum = tsum + el;
@@ -19,5 +18,8 @@ function sum_arr(arr){
     return tsum;
 }
 
-console.log(sum_arr(t_arr));
-document.write('<p>'+'summary : '+sum_arr(t_arr)+'</p>');
+let t_sum = sum_arr(t_arr);
+
+console.log('func output', sum_arr(t_arr));
+console.log('val output', t_sum);
+document.write('<p>'+'summary : '+t_sum+'</p>');
