2bbd890974ff0ac42499b8f6800c49b6726ba6ab
Alex-Davidoff <alex.davidoff0009@gmail.com>
2024-11-18 21:18:02 +0200

    L7 t1

---
+++ L7/task1_XjJuucOMR0/js/XjJuucOMR0.js
@@ -0,0 +1,21 @@
+class User {
+    constructor(id, uname, surname, email, phone) {
+        this.id = id;
+        this.uname = uname;
+        this.surname = surname;
+        this.email = email;
+        this.phone = phone;
+    }
+}
+
+function CreateArrOfUser (len){
+    let tarr = [];
+    for (let c = 0; c<len; c++){
+        tarr.push(new User(c+1,'user name '+(c+1),'user surname '+(c+1),'email'+(c+1)+'@gmail.com',380960000000+c+1));
+    }
+    return tarr;
+}
+
+let user_arr = new CreateArrOfUser(10);
+
+console.log(user_arr);
