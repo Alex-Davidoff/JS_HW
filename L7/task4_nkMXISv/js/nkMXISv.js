94c7e6f39c0d2c7d08c02a23dec96fb4a664fa80
Alex-Davidoff <alex.davidoff0009@gmail.com>
2024-11-19 09:32:29 +0200

    L7 t4 1

--- L7/task4_nkMXISv/js/nkMXISv.js
+++ L7/task4_nkMXISv/js/nkMXISv.js
@@ -1,24 +1,31 @@
 class Client{
-    constructor(id, cname, surname , email, phone, order){
+    constructor(id, cname, surname , email, phone, ...products){
         this.id = id;
         this.cname = cname;
         this.surname = surname;
         this.email = email;
         this.phone = phone;
-        this.order = order;
+        this.order = products;
     }
 }
 
-//+ order=array of object {title,pryce,amount} = function or class
+function Product(title,pryce,amount){
+        this.title = title;
+        this.pryce = pryce;
+        this.amount = amount;
+    }
 
-// let test_client = new Client(1, 'cl_name', 'cl_surname', 'cl_email', 380960001122, ['wine','chokolate','orange']);
-// console.log(test_client);
-/*
-function Clients_init (len){
+const client1 = new Client(1, 'test name 1', 'test surname 1', 'email1@gmail.com', 380961112233, new Product('wine',200,1),new Product('orange',160,10));
+console.log(client1);
+
+function Clients_fill (len){
     let tarr = [];
     for (let c=0; c<len; c++){
-        
-
+        let titem = new Client(1, 'test name 1', 'test surname 1', 'email1@gmail.com', 380961112233, [{title:'apple',pryce:100,amount:10},{title:'orange',pryce:160,amount:5}]);
+        tarr.push(titem);
     }
     return tarr;
-}*/
+}
+
+let clients = new Clients_fill(10);
+console.log(clients);
