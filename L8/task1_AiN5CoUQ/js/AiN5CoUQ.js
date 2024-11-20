b9958f15e53b44eb8290003a5ba4ce32567bd56c
Alex-Davidoff <alex.davidoff0009@gmail.com>
2024-11-20 22:12:41 +0200

    L8 t1 1

--- L8/task1_AiN5CoUQ/js/AiN5CoUQ.js
+++ L8/task1_AiN5CoUQ/js/AiN5CoUQ.js
@@ -12,26 +12,44 @@ let testobject = {
     },
     getAdress: function(){
         return this.adress_to.postindex+', '+this.adress_to.city+', '+this.adress_to.recipient;
+        //return 'Adress str';
+    },
+    road:{
+        roadindexes:[{roadindex:12015, check:false},
+                     {roadindex:12000, check:false},
+                     {roadindex:11000, check:false},
+                     {roadindex:11701, check:false}],
+        showRoad: function(){
+            let troad = 'Шлях посилки: ';
+            for (const step of this.roadindexes){
+                troad = troad.concat(step.roadindex, '/', step.check, ' ');
+            }
+            return troad.trimEnd();
+        }
     }
 }
 
 console.log(testobject);
 console.log(testobject.getAdress());
-console.log('------------');
-console.log(Array.isArray(testobject.transportAttrib));
+console.log(testobject.road.showRoad());
 
-console.log('------------');
 
 function createFullCopy(orig){
-    let tmpres;
+if (orig){    
+    let tmp = JSON.parse(JSON.stringify(orig));
     for (const attr in orig){
-        //console.log(Array.isArray(orig[attr]), attr);
-        let tmp = orig[attr];
-        //tmpres[attr] = tmp;
+        if (typeof orig[attr] === 'function'){
+            tmp[attr] = orig[attr].bind(tmp);
+        }
     }
-    return tmpres;
+    return tmp;
+} else {
+    console.log('createFullCopy error "original object is null"');
+}
 }
 
-
+console.log('------------ copy');
 const copytest = createFullCopy(testobject);
 console.log(copytest);
+console.log(copytest.getAdress());
+//console.log('copytest.road.showRoad()', 'not exists in copy'); //need upgrade (recursion)
