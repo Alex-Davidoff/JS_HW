9f5179eab44f126b55747f84c3a849153f627ab8
Alex-Davidoff <alex.davidoff0009@gmail.com>
2024-11-20 16:23:44 +0200

    L8 t2

--- L8/task2_iz6emEsP2BA/js/iz6emEsP2BA.js
+++ L8/task2_iz6emEsP2BA/js/iz6emEsP2BA.js
@@ -6,4 +6,10 @@ let coursesAndDurationArray = [
     {title: 'FullStack', monthDuration: 7},
     {title: 'Frontend', monthDuration: 4}
 ];
+console.log(coursesAndDurationArray);
 
+
+let remapedCoursesAndDurationArray = coursesAndDurationArray.map((value, index) => {return {id:index+1, title:value.title, monthDuration:value.monthDuration}})
+console.log(remapedCoursesAndDurationArray);
+
+console.log(coursesAndDurationArray[0].title === remapedCoursesAndDurationArray[0].title);
