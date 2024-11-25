a99033a2b35541275bf678dbfde64fc537da4850
Alex-Davidoff <alex.davidoff0009@gmail.com>
2024-11-25 21:06:13 +0200

    L10 t4 1

--- L10/task4_2VaLt4vDczH/js/2VaLt4vDczH.js
+++ L10/task4_2VaLt4vDczH/js/2VaLt4vDczH.js
@@ -1,9 +1,9 @@
 let counter_storage = document.getElementById('counter');
 let c = localStorage.getItem('test_c') || 0;
-counter_storage.innerText = c;
-localStorage.setItem('test_c',++c);
+counter_storage.innerText = ++c;
+localStorage.setItem('test_c',c);
 
 document.getElementById('butt_reset').onclick = function (){
-    localStorage.setItem('test_c', 1);
+    localStorage.setItem('test_c', 0);
     counter_storage.innerText = 0;
 }
